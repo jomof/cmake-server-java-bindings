b3205a8bbf456bee9e0bcee00633ac8d9f698df6
Jomo Fisher <jomof@google.com>
2016-12-16 14:18:27 -0800

    More exists

--- com/jomofisher/cmakeserver/CMakeServerConnection.java
+++ com/jomofisher/cmakeserver/CMakeServerConnection.java
@@ -139,10 +139,6 @@ public class CMakeServerConnection {
 
   public HandshakeReplyMessage handshake(String cookie, File sourceDirectory,
       File buildDirectory, String generator) throws IOException {
-    if (!new File(sourceDirectory, "CMakeLists.txt").exists()) {
-      throw new RuntimeException(String.format(
-          "Expected sourceDirectory %s to contain CMakeLists.txt", sourceDirectory));
-    }
     return handshake(String.format(
         "{\"cookie\":\"%s\", "
             + "\"type\":\"handshake\", "
