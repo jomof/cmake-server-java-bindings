3064dd76986576efb630cf38b022fcf34d9d990c
Jomo Fisher (windows) <jomofisher@gmail.com>
2016-12-17 20:47:47 -0800

    Apache

--- src/main/java/com/jomofisher/cmakeserver/model/MessageMessage.java
+++ src/main/java/com/jomofisher/cmakeserver/model/MessageMessage.java
@@ -1,16 +1,20 @@
+/*
+ * Copyright 2016 Jomo Fisher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jomofisher.cmakeserver.model;
 
-/**
- * Example,
- * <p>
- * {
- * "cookie":"",
- * "inReplyTo":"configure",
- * "message":"CMake Error: CMake was unable to find a build program corresponding to \"Ninja\".  CMAKE_MAKE_PROGRAM is not set.  You probably need to select a different build tool.",
- * "title":"Error",
- * "type":"message"
- * }
- */
 @SuppressWarnings("unused")
 public class MessageMessage extends BaseMessage {
     public String cookie;
