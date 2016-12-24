40da055b94afa4db5f338b323f9940066cb680df
Jomo Fisher (windows) <jomofisher@gmail.com>
2016-12-24 11:08:48 -0800

    Android studio JSON model

--- src/test/java/com/jomofisher/tests/cmake/TestCMakeServer.java
+++ src/test/java/com/jomofisher/tests/cmake/TestCMakeServer.java
@@ -26,8 +26,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -306,7 +308,13 @@ public class TestCMakeServer {
                 "-DCMAKE_CXX_FLAGS=");
 
         setUpCmakeEnvironment(processBuilder.environment());
+        processBuilder.redirectErrorStream();
         Process process = processBuilder.start();
+        BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println("android-studio-out: " + line);
+        }
         assertThat(process.waitFor()).named("Android CMake process").isSameAs(0);
         Thread.sleep(1000);
 
